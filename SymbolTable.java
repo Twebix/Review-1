6d43cf994d802483c134ed32b0fd5fb71708895f
sandipon-ctrl <cse_0182310012101204@lus.ac.bd>
2026-05-23 12:03:50 +0600

    Added java files

--- SymbolTable.java
+++ SymbolTable.java
@@ -1,20 +1,38 @@
+package compiler;
+
 import java.util.HashMap;
+import java.util.Map;
+
 
 public class SymbolTable {
-    HashMap<String, Integer> table = new HashMap<>();
 
-    public void set(String key, int value) {
-        table.put(key, value);
+    private final Map<String, String> table  = new HashMap<>();
+    final SymbolTable         parent;
+
+    public SymbolTable(SymbolTable parent) {
+        this.parent = parent;
     }
 
-    public int get(String key) {
-        return table.getOrDefault(key, 0);
+    public void define(String name, String type) {
+        table.put(name, type);
     }
 
-    public void printTable() {
-        System.out.println("\n--- SYMBOL TABLE ---");
-        for (String key : table.keySet()) {
-            System.out.println(key + " = " + table.get(key));
-        }
+    public String lookup(String name) {
+        if (table.containsKey(name)) return table.get(name);
+        if (parent != null)          return parent.lookup(name);
+        return null;
+    }
+
+    public boolean isDefined(String name) {
+        return lookup(name) != null;
+    }
+
+    public boolean isDefinedLocally(String name) {
+        return table.containsKey(name);
+    }
+
+    @Override
+    public String toString() {
+        return "SymbolTable" + table;
     }
 }
